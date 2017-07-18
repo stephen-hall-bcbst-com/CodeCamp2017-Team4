be5f39063eea1857cbfe8097e4ae1990113bbc99
Bella Blankenbaker <22.blankenbaker.isabella@csasupper.com>
2017-07-18 14:58:04 -0400

    commit

--- data/CodeCamp-Help.js
+++ data/CodeCamp-Help.js
@@ -12,7 +12,7 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        Slack.postMessageToChannel(channelName, 'Heard your cry for help.');
+        Slack.postMessageToChannel(channelName, 'I heard you needed help\n. With me you have some options!First ask me a question. You can ask me to add or even what the weather is!I can tell you quotes that are inspiring and also make you laugh with some jokes. When you are ready type what you want to tell or ask me in the message bar and I will respond as soon as I have the answer.Enjoy!');
 
         // *********************************************************************
         // STOP CODING!
