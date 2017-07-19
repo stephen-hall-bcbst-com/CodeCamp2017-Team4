5446cd1f49a012e70dffa6fc20bbcfcc25ac3599
Nicholas Turitto <ncturitto@gmail.com>
2017-07-19 12:51:08 -0400

    change question

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -15,7 +15,7 @@ module.exports = {
         var response = "I don't understand the question...";
        
         // what to answer when asked about weather 
-        if (qlc.includes("how") && qlc.includes("weather")) {
+        if (qlc.includes("what") && qlc.includes("weather")) {
          response = "Today is partly cloudy with a high temperature of 96 degrees and a low of 73";
      
       // what to answer when asked why is the sky blue 
