5edc232dba168ae84773b9b4792ba6a564b54010
Gabe Turitto <gabet22704@gmail.com>
2017-07-19 11:51:15 -0400

    bug fix

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -18,24 +18,33 @@ module.exports = {
         var greetings = 'Hi there';
         var thanks = 'Thank you';
         var joke = 'What do you call a fake noodle? an impasta.';
+        var rap = 'What is rapping? i was born 3 days ago';
+
+        // randomizer for jokes
         
+        
+        var 
 
 
-        // The bots responses
+
+
+
+
+        // The bots responses to things you say
         if (message.includes('Hello')) {
-             Slack.postMessageToChannel(channelName, greetings);
+            Slack.postMessageToChannel(channelName, greetings);
         }
 
         if (message.includes('hello')) {
-             Slack.postMessageToChannel(channelName, greetings);
+            Slack.postMessageToChannel(channelName, greetings);
         }
 
         if (message.includes('Good job')) {
-             Slack.postMessageToChannel(channelName, thanks);
+            Slack.postMessageToChannel(channelName, thanks);
         }
 
         if (message.includes('good job')) {
-             Slack.postMessageToChannel(channelName, thanks);
+            Slack.postMessageToChannel(channelName, thanks);
         }
 
         if (message.includes('Thank you')) {
@@ -46,18 +55,23 @@ module.exports = {
             Slack.postMessageToChannel(channelName, grat);
         }
 
-
         if (message.includes('Tell me a joke')) {
             Slack.postMessageToChannel(channelName, joke);
         }
 
-         if (message.includes('tell me a joke')) {
+        if (message.includes('tell me a joke')) {
             Slack.postMessageToChannel(channelName, joke);
         }
 
-       
+        if (message.includes('Rap for me')) {
+            Slack.postMessageToChannel(channelName, rap);
+        }
 
+        if (message.includes('rap for me')) {
+            Slack.postMessageToChannel(channelName, rap);
+        }
 
+        
 
         
         
