a5a08e294a7bb5eacf6217e1a20218e6997c9325
Bella Blankenbaker <22.blankenbaker.isabella@csasupper.com>
2017-07-19 12:34:08 -0400

    memory of weather

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -12,13 +12,19 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-         
+         var memory = require('./CodeCamp-help.js');
+
          var response = "I heard you needed help! I can answer simple questions you have about how I work. You can also ask me questions about math, the weather, and I even tell jokes and quotes.Just make sure to include a question mark when you ask a question.";
         
         // used when message says "help" and specifies that they need help with how to find out the weather;
         if (message.includes("weather")) {
             response = "If you type your question about the weather in the message bar, I can tell you what the weather is today!";
-       
+            
+            if (memory.askedAboutWeather == true) {
+                response = "You have already asked this question.";
+            }
+            
+            memory.askedAboutWeather = true;
         // used when message says "help" and specifies that they need help with how to ask the bot to tell a joke;
         } else if (message.includes("joke")) {
             response = "If you say to the bot to 'tell me joke' in the message bar it is sure to make you laugh.";
--- CodeCamp-Memory.js
+++ CodeCamp-Memory.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var botData = require('./data/bot');
+var askedAboutWeather = false;
+
 
 module.exports = {
     botData: botData,
+    askedAboutWeather: askedAboutWeather,
 };
