6c77a86772808683bbe09b0bbfe172494301b541
Gabe Turitto <gabet22704@gmail.com>
2017-07-19 13:47:18 -0400

    randomizer for quotes

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -18,6 +18,16 @@ module.exports = {
         var greetings = 'Hi there';
         var thanks = 'Thank you';
         var rap = 'What is rapping? i was born 3 days ago';
+        
+
+
+        // randomizer for quotes
+        var quotes = ['Dont cry because it is over smile because it happened.', 'Dwell on the beauty of life watch the stars and see yourself running with them.', 'Those who wish to sing always find a song.', 'Every problem has a gift for you in its hand', 'Everything great was accomplished by fools who dream'];
+        var RQ = Math.floor((Math.random() * 5));
+        if (message.includes('Tell me a quote')) {
+            Slack.postMessageToChannel(channelName, quotes[RQ]);
+        }
+
 
         // randomizer for jokes
         var jokes = ["What do you call a fake noodle? an impasta.", "How does a penguin build it’s house? Igloos it together.", "What did the ocean say to the shore? Nothing, it just waved.", "Why do crabs never give to charity? Because they’re shellfish.", "What do you call a man with no nose and no body? Nobody nose."];
@@ -64,6 +74,10 @@ module.exports = {
 
         if (message.includes('rap for me')) {
             Slack.postMessageToChannel(channelName, rap);
+        }
+
+        if (message.includes('Tell me a quote')) {
+            Slack.postMessageToChannel(channelName, quote)
         }
 
 
