b290c16a8b0c8dc7a800788ab61e9bcf0865a752
Nicholas Turitto <ncturitto@gmail.com>
2017-07-19 10:12:51 -0400

    updated

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -11,8 +11,19 @@ module.exports = {
         // CODE HERE! 
         // *********************************************************************
         
-        Slack.postMessageToChannel(channelName, "What a lovely day to go outside.");
+        var qlc = question.toLowerCase('How is the weather?');
+        var response = "I don't understand the question...";
         
+        if (qlc.includes("how") && qlc.includes("weather")) {
+            // handle weather question
+      } else if (qlc.includes("why") && qlc.includes("sky") && qlc.includes("blue")) {
+            // handle sky blue question
+        }
+        var qlc = ('');
+        var response = ('');
+        
+       
+        Slack.postMessageToChannel(channelName, response);
 
         // *********************************************************************
         // STOP CODING!
