299a1ffa601b8986d42163c686624eb3ebf1493e
Nicholas Turitto <ncturitto@gmail.com>
2017-07-19 12:48:08 -0400

    added and changed questoins

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -13,18 +13,23 @@ module.exports = {
         
         var qlc = question.toLowerCase('How is the weather?');
         var response = "I don't understand the question...";
-        
+       
+        // what to answer when asked about weather 
         if (qlc.includes("how") && qlc.includes("weather")) {
-            // handle weather question
-      } else if (qlc.includes("why") && qlc.includes("sky") && qlc.includes("blue")) {
+         response = "Today is partly cloudy with a high temperature of 96 degrees and a low of 73";
+     
+      // what to answer when asked why is the sky blue 
+    } else if (qlc.includes("why") && qlc.includes("sky") && qlc.includes("blue")) {
             // handle sky blue question
         }
-        var qlc = ('');
-        var response = ('');
-        
-       
+            // what to answer when asked about birthday 
+        if (qlc.includes("what") && qlc.includes("birthday"));
+        response= "7/18/17";
+             
+              // calls out function
         Slack.postMessageToChannel(channelName, response);
 
+        
         // *********************************************************************
         // STOP CODING!
         // *********************************************************************
