0a974a8bcc752252a282bf07de47ec4275bc204c
Gabe Turitto <gabet22704@gmail.com>
2017-07-20 09:39:27 -0400

    birthstone function

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -20,9 +20,25 @@ module.exports = {
         var greetings = 'Hi there';
         var thanks = 'Thank you';
         var rap = 'What is rapping? i was born 3 days ago';
+        var stone = 'What is your birth month?';
         
-
-
+        // variables for birthstone function
+        var jan = 'garnet';
+        var feb = 'amethyst';
+        var march = 'aquamarine';
+        var april = 'diamond';
+        var may = 'emerald';
+        var june = 'pearl';
+        var july = 'ruby';
+        var august = 'peridot';
+        var september = 'sapphire';
+        var october = 'opal';
+        var november = 'topaz';
+        var december = 'tanzanite, zircon and turquoise';
+        
+        
+        
+        
         // randomizer for quotes
         var quotes = ['Dont cry because it is over smile because it happened.',
          'Dwell on the beauty of life watch the stars and see yourself running with them.', 
@@ -96,8 +112,65 @@ module.exports = {
             Slack.postMessageToChannel(channelName, rap);
         }
 
+        if (message.includes('Tell me my birthstone')) {
+            Slack.postMessageToChannel(channelName, stone);
+        }
         
+        if (message.includes('tell me my birthstone')) {
+            Slack.postMessageToChannel(channelName, stone);
+        }
 
+        
+        // birthstone telling machine or BTM
+        if (message.includes('January')) {
+            Slack.postMessageToChannel(channelName, jan);
+        }
+
+        if (message.includes('February')) {
+            Slack.postMessageToChannel(channelName, feb);
+        }
+
+        if (message.includes('March')) {
+            Slack.postMessageToChannel(channelName, march);
+        }
+
+        if (message.includes('April')) {
+            Slack.postMessageToChannel(channelName, april);
+        }
+
+        if (message.includes('May')) {
+            Slack.postMessageToChannel(channelName, may);
+        }
+
+        if (message.includes('June')) {
+            Slack.postMessageToChannel(channelName, june);
+        }
+
+        if (message.includes('July')) {
+            Slack.postMessageToChannel(channelName, july);
+        }
+
+        if (message.includes('August')) {
+            Slack.postMessageToChannel(channelName, august);
+        }
+
+        if (message.includes('September')) {
+            Slack.postMessageToChannel(channelName, september);
+        }
+
+        if (message.includes('October')) {
+            Slack.postMessageToChannel(channelName, october);
+        }
+
+        if (message.includes('November')) {
+            Slack.postMessageToChannel(channelName, november);
+        }
+
+        if (message.includes('December')) {
+            Slack.postMessageToChannel(channelName, december);
+        }
+        
+        
 
 
         
