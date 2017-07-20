9780b62b3d8b77b281c13c5ce7973994eedd7b8d
Gabe Turitto <gabet22704@gmail.com>
2017-07-20 11:38:38 -0400

    bored changes/ fix

--- CodeCamp-Bored.js
+++ CodeCamp-Bored.js
@@ -16,16 +16,12 @@ module.exports = {
         // *********************************************************************
         
         var bored = ["Where did you go? I tell jokes, quotes and I can even tell you the weather. You cant get bored with me!",
-        "Give me attention",
+        "Give me attention.",
         "Are you as bored as i am?",
-        "Being bored is an insult to ones self"];
+        "Being bored is an insult to ones self."];
 
-        Slack.postMessageToChannel(channelName, bored[memory.lastBoreTold]);
-            memory.lastBoreTold = memory.lastBoreTold + 1; 
-            if (memory.lastBoreTold == bored.length) {
-                memory.lastBoreTold = 0; 
-            }
-         }
+        var random = Math.floor((Math.random() * 3));
+        Slack.postMessageToChannel(channel, bored[random]);
 
 
         
