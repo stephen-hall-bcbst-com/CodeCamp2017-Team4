44010dc9d4331c7f02028a81ece3797b3fc266d7
Bella Blankenbaker <22.blankenbaker.isabella@csasupper.com>
2017-07-20 11:46:35 -0400

    change in help with memory

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -19,12 +19,11 @@ module.exports = {
         // used when message says "help" and specifies that they need help with how to find out the weather;
         if (message.includes("weather")) { 
             if (memory.askedAboutWeather == false) {
-            response = "If you type your question about the weather in the message bar, I can tell you what the weather is today!";
-            memory.askedAboutWeather = true;
-        
+                response = "If you type your question about the weather in the message bar, I can tell you what the weather is today!";
+                 memory.askedAboutWeather = true;
         // used when user has asked the same help question more than once  
-        } else if (memory.askedAboutWeather == true) {   
-        response = "You have already asked this question.";
+            } else if (memory.askedAboutWeather == true) {   
+                 response = "You have already recieved help regarding this subject.";
             } 
        
         // used when message says "help" and specifies that they need help with how to ask the bot to tell a joke;
@@ -33,28 +32,34 @@ module.exports = {
             
         // used when message says "help" and specifies that they need help with how to ask the bot to add two numbers;  
         } else if (message.includes("adding")) {
-            response = "If you ask me to add two numbers and include the numbers you would like to add with the plus sign inbetween, I will add your two values.";
-        // used if user asks the same help question more than once    
-         if (memory.askedAboutAdding == true) {
-                response = "You have already asked this question.";
-            }
+            if (memory.askedAboutAdding== false) {
+                response = "If you ask me to add two numbers and include the numbers you would like to add with the plus sign inbetween, I will add your two values.";
+                memory.askedAboutAdding = true;
+        // used when user has asked the same help question more than once 
+          } else if (memory.askedAboutAdding == true) {
+                response = " You have already recieved help regarding this subject."; 
+          }
        
         // used when message says "help" and specifies that they need help with how to tell to the bot to tell a quote;
         } else if (message.includes("Quote")) {
             response = "If you say to me  'Tell me a quote' I will respond with a quote. Just make sure to use a capital 'T'";
-           }
+        }
         
         // used when message says "help" and specifies that they need help with how to tell the bot to tell the user their birthstone;
          if (message.includes("birthstone")) {
-             response = "If you say to me 'Tell me my birthstone' I will respond with 'What is your birthmonth'. When you tell me your birthmonth I will then tell you your birthstone";
-         // used if user has asked the same help question about birthstie more than once
-         if (memory.askedAboutBirthstone == true) {
-            response = "You have already asked this question.";
+            if (memory.askedAboutBirthstone == false) {
+                 response = "If you say to me 'Tell me my birthstone' I will respond with 'What is your birthmonth'. When you tell me your birthmonth I will then tell you your birthstone";
+                 memory.askedAboutBirthstone = true;
+        // used when user has asked the same help message
+         } else if (memory.askedAboutBirthstone == false) {
+                response = " You have already recieved help regarding this subject.";
          }
-         }
+         }  
+        
+            
         
 
-            Slack.postMessageToChannel(channelName, response);
+        Slack.postMessageToChannel(channelName, response);
 
         // *********************************************************************
         // STOP CODING!
