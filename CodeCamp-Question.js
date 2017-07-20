61e47f9c0e401f256b0096a4ac6e58b93b9db94f
Nicholas Turitto <ncturitto@gmail.com>
2017-07-20 09:14:24 -0400

    change in question

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -11,21 +11,23 @@ module.exports = {
         // CODE HERE! 
         // *********************************************************************
         
-        var qlc = question.toLowerCase('How is the weather?');
+
+        
         var response = "I don't understand the question...";
-       
+               
         // what to answer when asked about weather 
-        if (qlc.includes("How is the weather today.")) {
-         response = "Today is partly cloudy with a high temperature of 96 degrees and a low of 73";
+    if (question.includes("how is the weather today?")) {
+        response = "today is partly cloudy with a high temperature of 96 degrees and a low of 73";
      
       // what to answer when asked why is the sky blue 
-    } else if (qlc.includes("why") && qlc.includes("sky") && qlc.includes("blue")) {
+    } if (question.includes("why") || question.includes("sky") || question.includes("blue")) {
+            response = "why is the sky blue."; 
             // handle sky blue question
-        }
+           
             // what to answer when asked about birthday 
-        if (qlc.includes("What is your birthday"));
-        response= "July 18th 2017";
-             
+    } if (question.includes("what is your birthday")) {
+             response = "July 18th 2017";
+    }
               // calls out function
         Slack.postMessageToChannel(channelName, response);
 
