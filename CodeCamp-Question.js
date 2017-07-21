e0896305609a7f82ee6133a1da7218a6d9a686fe
Bella Blankenbaker <22.blankenbaker.isabella@csasupper.com>
2017-07-21 10:16:17 -0400

    lowercase change

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -11,28 +11,28 @@ module.exports = {
         // CODE HERE! 
         // *********************************************************************
         
-
+        var lc = question.toLowerCase();
         
         var response = "I don't know the answer to that question. I'm not as smart as you.";
                
              // what to answer when asked about weather 
-    if (question.includes("How is the weather today?")) {
+    if (lc.includes("how is the weather today?")) {
         response = "Today is partly cloudy with a high temperature of 96 degrees and a low of 73.";
      
              // what to answer when asked why is the sky blue 
-    } if (question.includes("why") && question.includes("sky") && question.includes("blue")) {
+    } if (lc.includes("why") && lc.includes("sky") && lc.includes("blue")) {
             response = "Blue is scattered by air molecules when sunlight reaches Earth. That is why we see a blue sky."; 
            
 
             // what to answer when asked about birthday 
-    } if (question.includes("When is your birthday?")) {
+    } if (lc.includes("when is your birthday?")) {
              response = "My birthday is July 18th, 2017.";
             // Adding a array to add numbers
-    } if (question.includes("+") || question.includes("plus")) {
-        question = question.replace('?', '');
-        question = question.replace('+', ' ');
-        question = question.replace('plus', ' ');
-        var array = question.split(' ');
+    } if (lc.includes("+") || lc.includes("plus")) {
+        lc = lc.replace('?', '');
+        lc = lc.replace('+', ' ');
+        lc = lc.replace('plus', ' ');
+        var array = lc.split(' ');
         var result = 0;
         for (var x = 0; x < array.length; x++) {
             var num = parseInt(array[x]);
